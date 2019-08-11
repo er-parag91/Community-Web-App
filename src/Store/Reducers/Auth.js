776ec51f82dd6f39d70131b3823ac2645a71c371
ppatel81 <ppatel81@dxc.com>
2019-08-11 00:04:00 -0500

    Added redux and setup initial store

---
+++ src/Store/Reducers/Auth.js
@@ -0,0 +1,12 @@
+const initialState = {
+  token: null,
+  email: null,
+  name: null,
+  error: null,
+  loading: false,
+};
+
+
+const authReducer = (state = initialState) => state;
+
+export default authReducer;
--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import {
+  createStore, applyMiddleware, compose, combineReducers,
+} from 'redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import authReducer from './Store/Reducers/Auth';
 
+const rootReducer = combineReducers({
+  user: authReducer,
+});
+const composeEnhancers = process.env.NODE_ENV === 'development'
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  : null || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk)),
+);
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 ReactDOM.render(app, document.getElementById('root'));
 
