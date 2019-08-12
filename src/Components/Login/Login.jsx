0b961118570fdb9d921c9b86c75479f3ac7410c4
ppatel81 <ppatel81@dxc.com>
2019-08-11 23:27:01 -0500

    Sign-Up Page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import './Login.scss';
+import '../../Typography-UI/Header.scss';
+import SignUp from './SignUp';
+
+const Logo = require('../../assets/logo__big.png');
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
       login: {},
-      signUp: {},
+      signUp: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        phone: '',
+        state: 'AK',
+        agree: false,
+      },
       width: null,
+      loggingIn: false,
     };
   }
 
@@ -18,25 +32,79 @@ class Login extends Component {
     window.addEventListener('resize', this.updateDimensions.bind(this));
   }
 
+  loginTypeHandler = () => {
+    this.setState((prevState) => ({
+      loggingIn: !prevState.loggingIn,
+    }));
+  }
+
+  textChangedHandler = (key, value) => {
+    console.log(key, value);
+    this.setState((prevState) => {
+      const { signUp } = prevState;
+      signUp[key] = value;
+      return { SignUp };
+    });
+  }
+
+  checkboxChangedHandler = () => {
+    this.setState((prevState) => {
+      const { signUp } = prevState;
+      signUp.agree = !signUp.agree;
+      return { SignUp };
+    });
+  }
+
   updateDimensions() {
     this.setState({ width: window.innerWidth });
   }
 
+
   render() {
     console.log(this.state);
-    console.log(window.innerWidth);
-    const { width } = this.state;
+    const { width, loggingIn, signUp } = this.state;
     return (
-      <Grid container>
+      <Grid item container className="login">
         {width > 601 && (
-        <Grid item xs={1} sm={2} md={4} lg={6}>
+        <Grid item xs={1} sm={4} md={8} lg={8}>
           <div className="login__left">
             <div className="login__left--image" />
           </div>
         </Grid>
         )}
-        <Grid item xs={12} sm={10} md={8} lg={6}>
-          <div>xs=12 sm=6</div>
+        <Grid item xs={12} sm={8} md={4} lg={4}>
+          <Grid
+            className="login__right"
+            container
+            direction="column"
+            justify="space-around"
+            alignItems="center"
+          >
+            <div className="login__right--logo">
+              <img className="Logo" src={Logo} alt="Logo" />
+            </div>
+
+            {loggingIn && (
+            <div className="login__right--signin">
+              <span>Login</span>
+            </div>
+            )}
+
+            {!loggingIn
+            && (
+            <SignUp
+              userData={signUp}
+              onTextChange={this.textChangedHandler}
+              onCheckboxChange={this.checkboxChangedHandler}
+            />
+            )}
+
+            <div>
+              <span role="button" tabIndex={0} className="heading-tertiary span__click" onClick={this.loginTypeHandler} onKeyDown={this.loginTypeHandler}>
+                {loggingIn ? 'New User? Tap to Sign Up' : 'Registered User? Tap to Log in'}
+              </span>
+            </div>
+          </Grid>
         </Grid>
       </Grid>
     );
---
+++ src/Components/Login/SignUp.jsx
@@ -0,0 +1,183 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import '../../Typography-UI/Header.scss';
+import '../../Typography-UI/Button.scss';
+import PropTypes from 'prop-types';
+import { statesList } from './statesList';
+
+const SignUp = (props) => {
+  const changeHandler = (key, target) => {
+    props.onTextChange(key, target.value);
+  };
+
+  const checkBoxHandler = () => {
+    props.onCheckboxChange();
+  };
+
+  const { userData } = props;
+  const {
+    firstName, lastName, email, password, confirmPassword, phone, state, agree,
+  } = userData;
+  return (
+    <div className="login__right--signup">
+      <Grid
+        container
+        direction="column"
+        justify="space-around"
+        alignItems="center"
+        className="login__right--signup-form"
+      >
+        <span className="heading-secondary">sign-up</span>
+        <Grid container spacing={1}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              value={firstName}
+              id="firstName"
+              label="First Name"
+              autoComplete="first-name"
+              placeholder="e.g. John"
+              required
+              fullWidth
+              variant="filled"
+              className="Form-Input"
+              onChange={(e) => changeHandler('firstName', e.target)}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              value={lastName}
+              id="lastName"
+              label="Last Name"
+              placeholder="e.g. Smith"
+              autoComplete="family-name"
+              required
+              fullWidth
+              variant="filled"
+              className="Form-Input"
+              onChange={(e) => changeHandler('lastName', e.target)}
+            />
+          </Grid>
+        </Grid>
+        <TextField
+          value={email}
+          id="email"
+          label="E-Mail Address"
+          placeholder="e.g. john@smith.com"
+          autoComplete="email"
+          required
+          fullWidth
+          type="email"
+          variant="filled"
+          className="Form-Input"
+          onChange={(e) => changeHandler('email', e.target)}
+        />
+        <Grid container spacing={1}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              value={password}
+              id="password"
+              label="Password"
+              placeholder="e.g. Password123"
+              autoComplete="new-password"
+              required
+              fullWidth
+              type="password"
+              variant="filled"
+              className="Form-Input"
+              onChange={(e) => changeHandler('password', e.target)}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              value={confirmPassword}
+              id="confirmPassword"
+              label="Confirm Password"
+              placeholder="e.g. Password123"
+              autoComplete="new-password"
+              required
+              fullWidth
+              type="password"
+              variant="filled"
+              className="Form-Input"
+              onChange={(e) => changeHandler('confirmPassword', e.target)}
+            />
+          </Grid>
+        </Grid>
+        <Grid container spacing={1}>
+          <Grid item xs={8} sm={8}>
+            <TextField
+              value={phone}
+              id="phone"
+              label="Phone"
+              placeholder="e.g. 888-888-8888"
+              autoComplete="phone"
+              required
+              fullWidth
+              variant="filled"
+              className="Form-Input"
+              onChange={(e) => changeHandler('phone', e.target)}
+            />
+          </Grid>
+          <Grid item xs={4} sm={4}>
+            <TextField
+              value={state}
+              id="state"
+              select
+              label="State"
+              SelectProps={{
+                native: true,
+              }}
+              required
+              fullWidth
+              variant="filled"
+              InputLabelProps={{ shrink: true }}
+              onChange={(e) => changeHandler('state', e.target)}
+            >
+              {statesList.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid xs={12} sm={12}>
+            <FormControlLabel
+              control={(
+                <Checkbox
+                  onChange={checkBoxHandler}
+                  checked={!!agree}
+                  id="agree"
+                />
+                  )}
+              label={(
+                <p className="terms__condition">
+                      I have read and accepted
+                  <span>terms and conditions.</span>
+                </p>
+                )}
+            />
+          </Grid>
+          <button type="submit" className="btn" style={{ margin: '2rem auto' }}>
+            <span className="btn__visible">Free Sign Up</span>
+          </button>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+SignUp.propTypes = {
+  userData: PropTypes.shape(),
+  onTextChange: PropTypes.func.isRequired,
+  onCheckboxChange: PropTypes.func.isRequired,
+
+};
+
+SignUp.defaultProps = {
+  userData: {},
+};
+
+export default SignUp;
