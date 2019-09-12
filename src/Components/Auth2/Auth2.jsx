02afd4d18bd7f8be381ab2b34a133afd5de52f20
ppatel81 <ppatel81@dxc.com>
2019-09-12 01:02:41 -0500

    finished login page - upcoming sign up page

--- src/Components/Auth2/Auth2.jsx
+++ src/Components/Auth2/Auth2.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/no-unused-prop-types */
+/* eslint-disable react/jsx-closing-bracket-location */
+/* eslint-disable react/prefer-stateless-function */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/destructuring-assignment */
 import React, { Component } from 'react';
@@ -10,152 +13,155 @@ import Login from './Login';
 const SVG = require('../../assets/SVGs/SVG.svg');
 
 class Auth2 extends Component {
-  state = {
-    login: {
-      currentEmail: '',
-      currentPassword: '',
-      rememberMe: false,
-    },
-    signUp: {
-      firstName: '',
-      lastName: '',
-      email: this.props.generalState.signUpEmail,
-      password: '',
-      confirmPassword: '',
-      phone: '',
-      state: 'AK',
-      agree: false,
-    },
-    isLoggingIn: this.props.generalState.isLoggingIn,
-    open: false,
-    dialog: false,
-    isForgotPassword: false,
-  };
+  // state = {
+  //   login: {
+  //     currentEmail: '',
+  //     currentPassword: '',
+  //     rememberMe: false,
+  //   },
+  //   signUp: {
+  //     firstName: '',
+  //     lastName: '',
+  //     email: this.props.generalState.signUpEmail,
+  //     password: '',
+  //     confirmPassword: '',
+  //     phone: '',
+  //     state: 'AK',
+  //     agree: false,
+  //   },
+  //   isLoggingIn: this.props.generalState.isLoggingIn,
+  //   open: false,
+  //   dialog: false,
+  //   isForgotPassword: false,
+  // };
 
-    // toggle between sign and login mode
-    loginTypeHandler = () => {
-      this.setState((prevState) => ({
-        isLoggingIn: !prevState.isLoggingIn,
-      }));
-    }
+  //   // toggle between sign and login mode
+  //   loginTypeHandler = () => {
+  //     this.setState((prevState) => ({
+  //       isLoggingIn: !prevState.isLoggingIn,
+  //     }));
+  //   }
 
-    //
-    // // Login
-    // // login text change handler
-    //
-    loginTextChangeHandler = (key, value) => {
-      console.log(key, value);
-      this.setState((prevState) => {
-        const { login } = prevState;
-        login[key] = value;
-        return { login };
-      });
-    }
+  //   //
+  //   // // Login
+  //   // // login text change handler
+  //   //
+  //   loginTextChangeHandler = (key, value) => {
+  //     console.log(key, value);
+  //     this.setState((prevState) => {
+  //       const { login } = prevState;
+  //       login[key] = value;
+  //       return { login };
+  //     });
+  //   }
 
-    // checkbox on login page, for rememer me checkbox check handler
-    rememberMeClicked = () => {
-      this.setState((prevState) => {
-        const { login } = prevState;
-        login.rememberMe = !login.rememberMe;
-        return { login };
-      });
-    }
+  //   // checkbox on login page, for rememer me checkbox check handler
+  //   rememberMeClicked = () => {
+  //     this.setState((prevState) => {
+  //       const { login } = prevState;
+  //       login.rememberMe = !login.rememberMe;
+  //       return { login };
+  //     });
+  //   }
 
-    // Forgot password dialog handler - toggle the dialog
-    forgotPasswordHandler = () => {
-      this.setState((prevState) => ({
-        isForgotPassword: !prevState.isForgotPassword,
-      }));
-    };
+  //   // Forgot password dialog handler - toggle the dialog
+  //   forgotPasswordHandler = () => {
+  //     this.setState((prevState) => ({
+  //       isForgotPassword: !prevState.isForgotPassword,
+  //     }));
+  //   };
 
-    // Log in form submit handler
-    logInFormSubmitHandler = (e) => {
-      e.preventDefault();
-      console.log(e);
-    };
+  //   // Log in form submit handler
+  //   logInFormSubmitHandler = (e) => {
+  //     e.preventDefault();
+  //     console.log(e);
+  //   };
 
-    // forgot password submit handler
-    onForgotPasswordSubmit = (e) => {
-      e.preventDefault();
-      console.log(e);
-    }
+  //   // forgot password submit handler
+  //   onForgotPasswordSubmit = (e) => {
+  //     e.preventDefault();
+  //     console.log(e);
+  //   }
 
-    //
-    // // sign up inputs change handler and changes the state value accordingly
-    //
-    textChangedHandler = (key, value) => {
-      this.setState((prevState) => {
-        const { signUp } = prevState;
-        signUp[key] = value;
-        return { signUp };
-      });
-    }
+  //   //
+  //   // // sign up inputs change handler and changes the state value accordingly
+  //   //
+  //   textChangedHandler = (key, value) => {
+  //     this.setState((prevState) => {
+  //       const { signUp } = prevState;
+  //       signUp[key] = value;
+  //       return { signUp };
+  //     });
+  //   }
 
-    // checkbox on sign-up page, for terms and conditions agree/not-agree handler
-    checkboxChangedHandler = () => {
-      this.setState((prevState) => {
-        const { signUp } = prevState;
-        signUp.agree = !signUp.agree;
-        return { signUp };
-      });
-    }
+  //   // checkbox on sign-up page, for terms and conditions agree/not-agree handler
+  //   checkboxChangedHandler = () => {
+  //     this.setState((prevState) => {
+  //       const { signUp } = prevState;
+  //       signUp.agree = !signUp.agree;
+  //       return { signUp };
+  //     });
+  //   }
 
-    // more info icon click opens up modal
-    onMoreInfoModalToggle = () => {
-      this.setState((prevState) => ({
-        open: !prevState.open,
-      }));
-    };
+  //   // more info icon click opens up modal
+  //   onMoreInfoModalToggle = () => {
+  //     this.setState((prevState) => ({
+  //       open: !prevState.open,
+  //     }));
+  //   };
 
-    // terms and condition modal toggler
-    toggleTermsAndConditionModal = () => {
-      this.setState((prevState) => ({
-        dialog: !prevState.dialog,
-      }));
-    };
+  //   // terms and condition modal toggler
+  //   toggleTermsAndConditionModal = () => {
+  //     this.setState((prevState) => ({
+  //       dialog: !prevState.dialog,
+  //     }));
+  //   };
 
-    // checkbox checked/not-checked changes the state value
-    agreedHandler = () => {
-      this.setState((prevState) => {
-        const { signUp } = prevState;
-        let { dialog } = prevState;
-        signUp.agree = true;
-        dialog = false;
-        return { signUp, dialog };
-      });
-    }
+  //   // checkbox checked/not-checked changes the state value
+  //   agreedHandler = () => {
+  //     this.setState((prevState) => {
+  //       const { signUp } = prevState;
+  //       let { dialog } = prevState;
+  //       signUp.agree = true;
+  //       dialog = false;
+  //       return { signUp, dialog };
+  //     });
+  //   }
 
-    // Sign up form submit handler
-    signUpSubmitHandler = (e) => {
-      e.preventDefault();
-      console.log(e);
-    }
+  //   // Sign up form submit handler
+  //   signUpSubmitHandler = (e) => {
+  //     e.preventDefault();
+  //     console.log(e);
+  //   }
 
-    render() {
-      const {
-        isLoggingIn, signUp, login, open, dialog, isForgotPassword,
-      } = this.state;
-      return (
-        <div className="Auth">
-          <div className="Auth__Container">
-            <div className="Auth__Container--SVG">
-              <img src={SVG} alt="" />
-            </div>
+  render() {
+  //     const {
+  //       isLoggingIn, signUp, login, open, dialog, isForgotPassword,
+  //     } = this.state;
+    const isLoggingIn = true;
+    return (
+      <div className="Auth">
+        <div className="Auth__Container">
+          <div className="Auth__Container--SVG">
+            <img src={SVG} alt="" />
+          </div>
+          <div className="Auth__Container--Login">
             {isLoggingIn && (
               <Login
-                loginData={login}
-                onLoginFormSubmit={this.logInFormSubmitHandler}
-                onLoginTextChange={this.loginTextChangeHandler}
-                onRememberMeClick={this.rememberMeClicked}
-                isForgotPassword={isForgotPassword}
-                onForgotPasswordClicked={this.forgotPasswordHandler}
-                forgotPasswordSubmitHandler={this.onForgotPasswordSubmit}
+                // loginData={login}
+                // onLoginFormSubmit={this.logInFormSubmitHandler}
+                // onLoginTextChange={this.loginTextChangeHandler}
+                // onRememberMeClick={this.rememberMeClicked}
+                // isForgotPassword={isForgotPassword}
+                // onForgotPasswordClicked={this.forgotPasswordHandler}
+                // forgotPasswordSubmitHandler={this.onForgotPasswordSubmit}
               />
             )}
           </div>
         </div>
-      );
-    }
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
--- src/Components/Auth2/Login.jsx
+++ src/Components/Auth2/Login.jsx
@@ -1,114 +1,93 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
-import Grid from '@material-ui/core/Grid';
-import InputField from '../Auth/TextField';
 import ForgotPassword from '../Auth/ForgotPassword';
 import '../../UI/Button.scss';
+import './Login.scss';
+import '../../UI/Header.scss';
 
-const Login = (props) => {
+const Avatar = require('../../assets/SVGs/Avatar.svg');
+
+class Login extends Component {
+  componentDidMount() {
+    const inputs = document.querySelectorAll('input');
+
+    inputs.forEach((input) => {
+      input.addEventListener('focus', function focusFunc() {
+        const parent = this.parentNode.parentNode;
+        parent.classList.add('focus');
+      });
+
+      input.addEventListener('blur', function blurFunc() {
+        const parent = this.parentNode.parentNode;
+        if (this.value === '') {
+          parent.classList.remove('focus');
+        }
+      });
+    });
+  }
+
   // Login text fields change fires this handler
-  const onChangeHandler = (key, target) => {
-    props.onLoginTextChange(key, target.value);
-  };
-  // remember me checkbox handler
-  const checkBoxHandler = () => {
-    props.onRememberMeClick();
-  };
+onChangeHandler = (key, target) => {
+  this.props.onLoginTextChange(key, target.value);
+};
 
-  // Handles login form submission
-  const handleSubmit = (e) => {
-    props.onLoginFormSubmit(e);
-  };
+// remember me checkbox handler
+checkBoxHandler = () => {
+  this.props.onRememberMeClick();
+};
 
-  // forgotpassword click handler - passes the props from chile to parent
-  const forgotPasswordClick = () => {
-    props.onForgotPasswordClicked();
-  };
+// Handles login form submission
+handleSubmit = (e) => {
+  this.props.onLoginFormSubmit(e);
+};
 
-  const forgotPasswordSubmit = (e) => {
-    props.forgotPasswordSubmitHandler(e);
-  };
-  const { loginData, isForgotPassword } = props;
+// forgotpassword click handler - passes the this.props from chile to parent
+forgotPasswordClick = () => {
+  this.props.onForgotPasswordClicked();
+};
+
+forgotPasswordSubmit = (e) => {
+  this.props.forgotPasswordSubmitHandler(e);
+};
+
+render() {
+  const { loginData, isForgotPassword } = this.props;
   const {
     currentEmail, currentPassword, rememberMe, onForgotPasswordClick,
   } = loginData;
   return (
-    <div className="login__right--signin">
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <Grid
-          container
-          direction="column"
-          justify="space-around"
-          alignItems="center"
-          className="login__right--signup-form"
-        >
-          <span className="heading-secondary">Welcome back!</span>
-          <InputField
-            type="email"
-            autoFocus
-            required
-            value={currentEmail}
-            id="currentEmail"
-            label="E-Mail Address"
-            placeholder="e.g. john@smith.com"
-            changeHandler={onChangeHandler}
-          />
-          <InputField
-            type="password"
-            required
-            inputProps={{
-              minLength: 8,
-            }}
-            value={currentPassword}
-            id="currentPassword"
-            label="Password"
-            placeholder="e.g. Password123"
-            changeHandler={onChangeHandler}
-          />
-          <Grid
-            xs={12}
-            sm={12}
-            container
-            direction="row"
-            justify="space-between"
-            alignItems="center"
-          >
-            <div style={{ marginTop: '1rem' }}>
-              <label htmlFor="rememberMe">
-                <input
-                  type="checkbox"
-                  onChange={checkBoxHandler}
-                  checked={!!rememberMe}
-                  id="rememberMe"
-                />
-                <span className="checkbox__mock"><i className="fa fa-check checkbox__mock--icon" aria-hidden="true" /></span>
-                <p className="rememberMe">
-                Remember Me
-                </p>
-              </label>
-            </div>
-            <div>
-              <span className="rememberMe" onClick={forgotPasswordClick}>
-                Forgot Password ?
-              </span>
-            </div>
-          </Grid>
-          <button type="submit" className="btn">
-            <span className="btn__text">Login</span>
-          </button>
-        </Grid>
-        <ForgotPassword
-          isForgotPassword={isForgotPassword}
-          onForgotPasswordToggle={forgotPasswordClick}
-          onForgotPasswordSubmit={forgotPasswordSubmit}
-        />
-      </form>
-    </div>
+    <form className="Login" onSubmit={this.handleSubmit}>
+      <img className="Login__Avatar" src={Avatar} alt="" />
+      <h2 className="heading-secondary" style={{ marginBottom: '3rem' }}>Welcome</h2>
+      <div className="Login__Input One">
+        <div className="Login__Input--Icon">
+          <i className="fa fa-user" />
+        </div>
+        <div className="Login__Input--Input">
+          <h5>Email Address</h5>
+          <input type="text" />
+        </div>
+      </div>
+      <div className="Login__Input Two">
+        <div className="Login__Input--Icon">
+          <i className="fa fa-lock" />
+        </div>
+        <div className="Login__Input--Input">
+          <h5>Password</h5>
+          <input type="password" />
+        </div>
+      </div>
+      <a href="#" type="button">Forgot Password?</a>
+      <input type="submit" className="Login__Button" value="Login" />
+    </form>
   );
-};
+}
+}
 
 Login.propTypes = {
   loginData: PropTypes.shape(),
